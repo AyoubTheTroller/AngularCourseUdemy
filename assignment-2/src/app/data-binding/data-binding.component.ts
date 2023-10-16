2e8b85a01d2f50abd18d307f144c89f769b23049
AyoubSaghir <ayoub.saghir@zupit.it>
2023-10-16 17:35:43 +0200

    Completed assignment-2

---
+++ assignment-2/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent {
+  username: string = '';
+
+  resetUsername(){
+    this.username = '';
+  }
+}
