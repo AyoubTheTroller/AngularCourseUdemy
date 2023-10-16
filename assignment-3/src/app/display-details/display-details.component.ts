6f259f9a34b7a8c428236bb707bfc20643c5fb85
AyoubSaghir <ayoub.saghir@zupit.it>
2023-10-16 19:38:20 +0200

    Completed assignment-3

---
+++ assignment-3/src/app/display-details/display-details.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-details',
+  templateUrl: './display-details.component.html',
+  styleUrls: ['./display-details.component.scss']
+})
+export class DisplayDetailsComponent {
+
+  isHidden: boolean = false;
+
+  buttonClicksCounter = 0;
+  logButtonClicks: string[] = [];
+
+  HideParaghraph() {
+    this.isHidden = !this.isHidden;
+    this.logButtonClicks.push("Click: " + this.buttonClicksCounter++);
+  }
+
+  shouldApplyBlueBg(log: string): boolean {
+    const position = this.logButtonClicks.indexOf(log);
+    return position >= 5;
+  }
+}
