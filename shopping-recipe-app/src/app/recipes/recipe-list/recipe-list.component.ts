bb32e49c439f9461c8e8a927898a1b896c77c970
AyoubSaghir <ayoub.saghir@zupit.it>
2023-10-17 13:04:05 +0200

    Finished Section 3 Created Base Project Structure for Shopping-recipe-app

---
+++ shopping-recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://sundaysuppermovement.com/wp-content/uploads/2019/07/picanha-steak-recipe-featured.jpg'),
+    new Recipe('A test recipe', 'This is simply a test', 'https://sundaysuppermovement.com/wp-content/uploads/2019/07/picanha-steak-recipe-featured.jpg'),
+    new Recipe('A test recipe', 'This is simply a test', 'https://sundaysuppermovement.com/wp-content/uploads/2019/07/picanha-steak-recipe-featured.jpg')   
+  ];
+}
---
+++ shopping-recipe-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+    /*public name: string;
+    public amount: number;
+
+    constructor(name: string, amount: number){
+        this.name = name;
+        this.amount = amount;
+    }*/
+    // OR Using shortcut below
+    // SHORTCUT: ASSIGNED DIRECTLY ONLY USING CONSTR
+
+    constructor(public name: string,public amount: number){}
+}
