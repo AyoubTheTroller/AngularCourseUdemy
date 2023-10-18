976deac5007e8e24fc5534f2aeeeedc0e0388b18
AyoubSaghir <ayoub50.as@gmail.com>
2023-10-18 12:33:27 +0200

    Completed assignment-4

--- assignment-4/src/app/app.component.ts
+++ assignment-4/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  valueEmitted: number;
+  onValueEmitted(value: number) {
+    this.valueEmitted = value;
+  }
   title = 'assignment-4';
 }
--- assignment-4/src/app/game-control/game-control.component.ts
+++ assignment-4/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-control.component.scss']
 })
 export class GameControlComponent {
+  valueEmitted: number = 0;
+  interval: any;
+  sum: number = 0;
 
+  constructor() { }
+
+  ngOnInit() { }
+
+  startGame() {
+    this.stopGame();
+    this.interval = setInterval(() => {
+      const randomValue = Math.floor(Math.random() * 100) + 1;
+      this.valueEmitted = randomValue;
+      this.sum = this.sum + randomValue;
+    }, 1000);
+  }
+
+  stopGame() {
+    clearInterval(this.interval);
+  }
+
+  // SECOND VERSION
+
+  @Output() valueEmitter = new EventEmitter<number>;
+
+  startEmittingValues() {
+    this.stopEmittingValues();
+    this.interval = setInterval(() => {
+      const randomValue = Math.floor(Math.random() * 100) + 1;
+      this.valueEmitted = randomValue;
+      this.sum = this.sum + randomValue;
+      this.valueEmitter.emit(randomValue);
+    }, 1000);
+  }
+
+  stopEmittingValues() {
+    clearInterval(this.interval);
+  }
 }
