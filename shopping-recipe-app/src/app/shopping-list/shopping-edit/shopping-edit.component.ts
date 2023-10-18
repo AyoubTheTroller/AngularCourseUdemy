91f4431e265a3e184f45118941304f720117dbe4
AyoubSaghir <ayoub50.as@gmail.com>
2023-10-18 22:29:46 +0200

    Sectin 6 completed

--- shopping-recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shopping-recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ShoppingEditComponent {
 
+  @Output() ingridient = new EventEmitter<Ingredient>();
+
+  onSubmit(ingridientName: HTMLInputElement, ingridientAmout: HTMLInputElement){
+    this.ingridient.emit(new Ingredient(ingridientName.value,ingridientAmout.valueAsNumber));
+  }
 }
--- shopping-recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ shopping-recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -7,8 +7,14 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+
   ingredients: Ingredient[] = [
     new Ingredient('Ananas',3),
     new Ingredient('Mango',5)
   ];
+  
+  addIngriedients(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+    console.log(ingredient);
+  }
 }
